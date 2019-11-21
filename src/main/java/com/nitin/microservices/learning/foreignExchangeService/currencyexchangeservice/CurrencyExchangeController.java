cd4cd03741b64dbc4362116bc3ee06eefa750f6b
Nitin Chaurasia <gs.nitin@gmail.com>
2019-11-20 22:40:25 -0500

    sleuth support for request id added

--- src/main/java/com/nitin/microservices/learning/foreignExchangeService/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/nitin/microservices/learning/foreignExchangeService/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.nitin.microservices.learning.foreignExchangeService.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,14 +23,16 @@ public class CurrencyExchangeController {
     @Autowired
     private ExchangeValueRepository exchangeValueRepository;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/foreign-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
-        //Caclling from H2 Database
+        //Calling from H2 Database
         ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from,to);
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 
+        logger.info("{}",exchangeValue);
         return exchangeValue;
-
     }
 
 
