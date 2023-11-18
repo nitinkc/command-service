6ad53b6c308257b15bcafc87807e5416098ff801
Nitin Chaurasia <gs.nitin@gmail.com>
2023-11-18 16:28:59 -0700

    updates

--- src/main/java/com/nitin/microservices/command/foreignExchangeService/CurrencyExchangeController.java
+++ src/main/java/com/nitin/microservices/command/foreignExchangeService/CurrencyExchangeController.java
@@ -4,9 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
@@ -40,9 +43,48 @@ public class CurrencyExchangeController {
         return ResponseEntity.ok(fromAndTo);
     }
 
+    //Get from mock
     @GetMapping("/foreign-exchange")
-    public String showGeneralMessage(){
+    public ResponseEntity<Map<String, Object>> showGeneralMessage(){
 
+        String url = "https://run.mocky.io/v3/2789a53b-401f-4f4f-a625-d513f547dfbf";
+        //delete : https://designer.mocky.io/manage/delete/2789a53b-401f-4f4f-a625-d513f547dfbf/nP7J9HbzF0K7USKVg4QDrVsT3l9HRCnKoN0I
+
+        // Define the type reference for the response
+        ParameterizedTypeReference<Map<String, Object>> responseType =
+                new ParameterizedTypeReference<Map<String, Object>>() {};
+
+        // Send a GET request and handle the response
+        ResponseEntity<Map<String, Object>> responseEntity = new RestTemplate().exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                responseType
+        );
+
+
+        // Check the HTTP status code
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            // HTTP status code is in the 2xx range (e.g., 200 OK)
+
+            // Access the response body as a Map
+            Map<String, Object> responseBody = responseEntity.getBody();
+
+            // You can now work with the responseBody Map
+            System.out.println("Response Body: " + responseBody);
+        } else {
+            // Handle non-successful HTTP status codes (4xx or 5xx) here
+            System.err.println("HTTP Request failed with status code: " + responseEntity.getStatusCode());
+        }
+        //Calling the Foreign Exchange Microservice from port 8000
+
+
+        return responseEntity;
+
+    }
+
+    @GetMapping("/")
+    public String welcomeMessage(){
         String msg = """
                 Use POST : http://localhost:8000/foreign-exchange
                 With Request Body as
@@ -52,11 +94,5 @@ public class CurrencyExchangeController {
                 }
                 """;
 
-        return msg;
-    }
-
-    @GetMapping("/")
-    public String welcomeMessage(){
-        return "welcome to command service";
-    }
+        return msg;    }
 }
